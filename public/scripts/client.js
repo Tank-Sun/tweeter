3af8047d5ab55ef07539b7c6dba37bcbfae9749f
Yongjia Sun <thomastank0926@gmail.com>
2022-10-13 08:01:40 +0000

    fetching tweets, and refetching tweets on submission by Ajax, also prevent XSS attack

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,91 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    })
+    .then(function(tweets) {
+      renderTweets(tweets);
+    })
+    .catch((error) => {
+      console.log('error:', error);
+    });
+  };
+
+  loadTweets();
+
+  const createTweetElement = function(tweet) {
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    };
+    const $tweet = $(`
+      <article>
+        <header>
+          <div class="user">
+            <div><img src=${tweet.user.avatars}></div>
+            <div>${tweet.user.name}</div>
+          </div>
+          <div class="at">${tweet.user.handle}</div>
+        </header>
+        <div class="tweet">${escape(tweet.content.text)}</div>
+        <footer>
+          <div>${timeago.format(Date.now() - tweet.created_at)}</div>
+          <div class="icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
+    return $tweet;
+  }
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    }
+  }
+
+  $("#new-tweet").submit(function(event){
+    event.preventDefault();
+    const $charLength = $(this).children('#tweet-text').val().length;
+    if ($charLength === 0) {
+      alert(`Empty tweet cannot be submitted.`);
+    } else if ($charLength > 140) {
+      alert(`The maximum message length is 140 characters.`);
+    } else {
+      const dataToSend = $(this).serialize();      
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: dataToSend
+      })
+      .then(() => {
+        $.ajax({
+          url: '/tweets',
+          method: 'GET'
+        })
+        .then(function(tweets) {
+          const $tweet = createTweetElement(tweets[tweets.length - 1]);
+          $('#tweets-container').prepend($tweet);
+        })
+        .catch((error) => {
+          console.log('error:', error);
+        });
+      })
+      .catch((error) => {
+        console.log('error:',error);
+      });
+    }
+  })
+
+});
+
